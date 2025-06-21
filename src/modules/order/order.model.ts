2834925826b36eb6b881afa8ad24b552bc04dc01
nahidn228 <nahidn228@gmail.com>
2025-06-21 13:02:35 +0600

    embedded orderAdressSchema added

--- src/modules/order/order.model.ts
+++ src/modules/order/order.model.ts
@@ -1,16 +1,24 @@
 import { Schema } from "mongoose";
 import { IOrder } from "./order.interface";
 
+const orderAddressSchema = new Schema({
+  zipCode: { type: String, required: true },
+  street: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
 const orderModel = new Schema<IOrder>({
   user: { type: Schema.Types.ObjectId },
   mango: { type: Schema.Types.ObjectId },
   quantity: { type: Number, min: 0, required: true },
   totalPrice: { type: Number, min: 0, required: true },
-  address: {
-    zipCode: { type: String, required: true },
-    street: { type: String, required: true },
-    state: { type: String, required: true },
-    country: { type: String, required: true },
-  },
+  // address: {
+  //   zipCode: { type: String, required: true },
+  //   street: { type: String, required: true },
+  //   state: { type: String, required: true },
+  //   country: { type: String, required: true },
+  // },
+  address: { type: orderAddressSchema, required: true  },
   status: { type: String, required: true },
 });
